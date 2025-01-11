c271f5480dba582a1496b7917b965ac03a641e7b
Larisa Turetskaia <lturetskaja@gmail.com>
2025-01-11 22:18:03 +0200

    Add initial point colouring

--- components/tools/bucketTool.js
+++ components/tools/bucketTool.js
@@ -3,5 +3,54 @@ class BucketTool extends ToolItem {
     super(name, icon);
   }
 
-  draw(){}
+  imagePixels;
+  newColour = [];
+  oldColour = [];
+
+  draw(colour) {
+    // set new and old colour value
+    this.newColour = colour;
+    this.oldColour = get(mouseX, mouseY);
+    console.log(`// Old colour: ${this.oldColour} 
+    // New colour: ${this.newColour} `);
+
+    //store current image
+    this.image = drawingContext.getImageData(0, 0, width, height);
+
+    this.floodFill(mouseX, mouseY);
+  }
+
+  floodFill(initialX, initialY) {
+    let currentPixel = { x: initialX, y: initialY };
+    // find index of the first pixel value (R)
+    let index = (currentPixel.x + currentPixel.y * width) * 4;
+    // colour the pixel
+    this.colourPixel(index);
+
+    //load the new image to the canvas
+    drawingContext.putImageData(this.image, 0, 0);
+
+    //create a queue for colouring pixels
+  }
+
+  findNeighbours(){
+    //find the neighbouring pixels of the current pixel (left, top, right, bottom)
+
+  }
+
+  colourPixel(index) {
+    const imagePixels = this.image.data; // access pixel array within image
+    let pixelIndex = index; // starting point
+
+    // find the four values of the pixel and change them to the corresponding new values
+    for (let i = 0; i < 4; i++) {
+      imagePixels[pixelIndex] = this.newColour[i];
+      pixelIndex++;
+    }
+  }
+ isValidPixel(){
+  // check if pixel is inside the canvas and has old colour
+
+ }
+
 }
--- sketch.js
+++ sketch.js
@@ -13,6 +13,11 @@ function setup() {
     canvasContainer.size().width,
     canvasContainer.size().height
   );
+  canvas.mouseClicked(() => {
+    if (toolbox.selectedTool.name === "bucket") {
+      toolbox.selectedTool.draw(colourPalette.selectedFillColour.rgba);
+    }
+  });
   background(255);
   canvas.parent("content");
   canvas.mouseReleased(() =>
@@ -56,6 +61,9 @@ function draw() {
   //if an object contains a particular method or property
   //if there isn't a draw method the app will alert the user
   if (toolbox.selectedTool.draw) {
+    if (toolbox.selectedTool.name === "bucket") {
+      return;
+    }
     toolbox.selectedTool.draw();
   } else {
     alert("it doesn't look like your tool has a draw method!");
