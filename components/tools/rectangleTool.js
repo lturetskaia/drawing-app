a781fd7337a051dd6622e275e94bb9726f561b1b
Larisa Turetskaia <lturetskaja@gmail.com>
2025-01-17 00:14:30 +0200

    Add size label to shapes

--- components/tools/rectangleTool.js
+++ components/tools/rectangleTool.js
@@ -7,19 +7,22 @@ class RectangleTool extends ToolItem {
   // set to default value -1
   startMouseX = -1;
   startMouseY = -1;
+  image;
 
   draw() {
     if (mouseIsPressed && mouseButton === LEFT) {
+      console.log(pixels);
       if (this.startMouseX === -1) {
         // initialize the starting point of a rectangle
         this.startMouseX = mouseX;
         this.startMouseY = mouseY;
-        // save the state of pixels
         loadPixels();
+        this.image = get(); // save the state of the canvas
+        // save the state of pixels
       } else {
         // display the last saved state of pixels
         updatePixels();
-        
+
         rect(
           this.startMouseX,
           this.startMouseY,
@@ -27,21 +30,34 @@ class RectangleTool extends ToolItem {
           mouseY - this.startMouseY
         );
         // display size of the area
-        // push();
-        // fill(0);
-        // noStroke();
-        // textSize(14);
-        // text(
-        //   `${mouseX - this.startMouseX} x ${mouseY - this.startMouseY}`,
-        //   mouseX + 5,
-        //   mouseY - 5
-        // );
-        // pop();
-
+        push();
+        fill(0);
+        noStroke();
+        textSize(14);
+        text(
+          `${mouseX - this.startMouseX} x ${mouseY - this.startMouseY}`,
+          mouseX + 5,
+          mouseY - 5
+        );
+        pop();
       }
     } else {
-      this.startMouseX = -1;
-      this.startMouseY = -1;
+      if (this.startMouseX !== -1) {
+        // when the drawing is done display the previous state before drawing
+        // this is needed to erase the dimesions label
+        image(this.image, 0, 0); 
+        // draw the final version of the rectangle
+        rect(
+          this.startMouseX,
+          this.startMouseY,
+          mouseX - this.startMouseX,
+          mouseY - this.startMouseY
+        );
+
+        this.startMouseX = -1;
+        this.startMouseY = -1;
+      }
+
     }
   }
 }
