fd113641c9357c50453fdfaf32e96b910d6a6764
Larisa Turetskaia <lturetskaja@gmail.com>
2025-01-17 13:49:00 +0200

    Add buttons to editImage options

--- components/tools/editImageTool.js
+++ components/tools/editImageTool.js
@@ -4,6 +4,35 @@ class EditImageTool extends ToolItem {
   }
 
   draw(){
+    // cursor(CROSS);
+
+  }
+
+  populateOptions(){
+    console.log('populating options');
+    this.addButton('copy');
+    this.addButton('delete');
+    this.addButton('cut');
+    this.addButton('paste');
+
+
+  }
+   addButton(name){
+    const icon = `assets/${name}.jpg`;
+    const newBtn = createButton("");
+    newBtn.id(`${name}Btn`);
+    select(".options").child(newBtn);
+
+    // add button icon
+    const buttonImg = createImg(icon, name);
+    buttonImg.id(`${name}Img`);
+    buttonImg.parent(`${name}Btn`);
+   }
+
+  unselectTool(){
+    console.log("Unselect edit image");
+    //clear options
+    select(".options").html("");
 
   }
 }
