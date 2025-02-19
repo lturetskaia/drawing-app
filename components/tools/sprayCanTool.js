1d2cdaa86cc72bd661c654e3084bcd8901c79a27
Larisa Turetskaia <lturetskaja@gmail.com>
2025-02-20 00:03:48 +0200

    Fix spray width

--- components/tools/sprayCanTool.js
+++ components/tools/sprayCanTool.js
@@ -1,22 +1,31 @@
 function SprayCanTool() {
   this.name = "sprayCanTool";
   this.icon = "assets/sprayCan.jpg";
-  this.label = 'Spray';
-  this.points = 40;
-  this.spread = 10;
+  this.label = "Spray";
+  this.minMaxPoints = [5, 500];
+
   this.draw = function () {
     //if the mouse is pressed paint on the canvas
     //spread describes how far to spread the paint from the mouse pointer
     //points holds how many pixels of paint for each mouse press.
     if (mouseIsPressed && mouseButton === LEFT) {
-      for (let i = 0; i < this.points; i++) {
-        push ();
+      const spread = strokeSlider.brushStrokeWeight / 2;
+      //map the number of points based on the stroke width
+      const points = map(
+        strokeSlider.brushStrokeWeight,
+        1,
+        100,
+        this.minMaxPoints[0],
+        this.minMaxPoints[1]
+      ).toFixed();
+      for (let i = 0; i < points; i++) {
+        push();
         strokeWeight(1);
         point(
-          random(mouseX - this.spread, mouseX + this.spread),
-          random(mouseY - this.spread, mouseY + this.spread)
+          random(mouseX - spread, mouseX + spread),
+          random(mouseY - spread, mouseY + spread)
         );
-        pop ();
+        pop();
       }
     }
   };
