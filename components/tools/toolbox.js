6a835d27c0aa69f7743f003cdbc796e9c664763f
Larisa Turetskaia <lturetskaja@gmail.com>
2025-02-23 00:16:15 +0200

    Refactor toolbox to class

--- components/tools/toolbox.js
+++ components/tools/toolbox.js
@@ -1,49 +1,50 @@
 //container object for storing the tools. Functions to add new tools and select a tool
-function Toolbox() {
-  const self = this;
-  this.tools = [];
-  this.selectedTool = null;
+class Toolbox {
+  constructor() {
+    this.tools = [];
+    this.selectedTool = null;
+  }
 
-  const toolbarItemClick = function () {
+  toolbarItemClick(item) {
     //remove any existing borders
     const items = selectAll(".sideBarItem");
     for (let i = 0; i < items.length; i++) {
       items[i].removeClass("active");
     }
 
-    const toolName = this.id().split("sideBarItem")[0];
-    self.selectTool(toolName);
+    const toolName = item.id().split("sideBarItem")[0];
+    this.#selectTool(toolName);
 
     //call loadPixels to make sure most recent changes are saved to pixel array
     loadPixels();
-  };
+  }
 
   //add a new tool icon to the html page
-  const addToolIcon = function (icon, name, label) {
+  #addToolIcon(icon, name, label) {
     const sideBarItem = createDiv(`<img src=${icon} alt = ${label} ></div>`);
     sideBarItem.class("sideBarItem");
     sideBarItem.id(name + "sideBarItem");
     const sidebar = select(".sidebar");
     sideBarItem.parent(sidebar);
-    sideBarItem.mouseClicked(toolbarItemClick);
-  };
+    sideBarItem.mouseClicked(() => this.toolbarItemClick(sideBarItem));
+  }
 
   //add a tool to the tools array
-  this.addTool = function (tool) {
+  addTool(tool) {
     //check that the object tool has an icon and a name
     if (!tool.icon || !tool.name) {
       alert("make sure your tool has both a name and an icon");
     }
     this.tools.push(tool);
-    addToolIcon(tool.icon, tool.name, tool.label);
+    this.#addToolIcon(tool.icon, tool.name, tool.label);
     //if no tool is selected (ie. none have been added so far)
     //make this tool the selected one.
     if (this.selectedTool == null) {
-      this.selectTool(tool.name);
+      this.#selectTool(tool.name);
     }
-  };
+  }
 
-  this.selectTool = function (toolName) {
+  #selectTool(toolName) {
     //search through the tools for one that's name matches the toolName
     for (let i = 0; i < this.tools.length; i++) {
       if (this.tools[i].name == toolName) {
@@ -57,7 +58,7 @@ function Toolbox() {
 
         //if the tool is eraser, set the stroke to previously selected value
         if (this.selectedTool.name === "eraser") {
-          strokeSlider.changeMode('eraser');
+          strokeSlider.changeMode("eraser");
         }
 
         //if the tool has an options area. Populate it now.
@@ -66,5 +67,5 @@ function Toolbox() {
         }
       }
     }
-  };
+  }
 }
